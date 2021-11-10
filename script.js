5952e16419464168d60281ef968c5806b6d0ba23
Dasuni Jayathilake <dasunijayathilake@gmail.com>
2021-11-10 13:07:38 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+
+}
+
+window.onscroll = () =>{
+
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+
+  if(window.scrollY > 60){
+    document.querySelector('#scroll-top').classList.add('active');
+  }else{
+    document.querySelector('#scroll-top').classList.remove('active');
+  }
+
+}
+
+function loader(){
+  document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut(){
+  setInterval(loader, 1500);
+}
+
+window.onload = fadeOut();
+
+
+var fullImgBox = document.getElementById("fullImgBox");
+var fullImg = document.getElementById("fullImg");
+
+function openFullImg(pic){
+  fullImgBox.style.display = "flex";
+  fullImg.src = pic;
+}
+
+function closeFullImg(pic){
+  fullImgBox.style.display = "none";
+}
